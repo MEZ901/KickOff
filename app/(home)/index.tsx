fb7c0820f64906396b44681f3aa977be3931cf7c
MEZ901 <issammez44@gmail.com>
2024-02-25 13:31:16 +0100

    create match details route

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,12 +1,40 @@
 import MatchCard from "@/components/MatchCard";
+import { router } from "expo-router";
 import { useState } from "react";
-import { Switch, Text, View } from "react-native";
+import { ScrollView, Switch, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+const matches = [
+  {
+    id: 1,
+    teamOneName: "Barcelona",
+    teamOneLogo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/4/47/FC_Barcelona_%28crest%29.svg/1200px-FC_Barcelona_%28crest%29.svg.png",
+    teamTwoName: "Manchester United",
+    teamTwoLogo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/800px-Manchester_United_FC_crest.svg.png",
+    time: "18:00",
+  },
+  {
+    id: 2,
+    teamOneName: "Barcelona",
+    teamOneLogo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/4/47/FC_Barcelona_%28crest%29.svg/1200px-FC_Barcelona_%28crest%29.svg.png",
+    teamTwoName: "Manchester United",
+    teamTwoLogo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/800px-Manchester_United_FC_crest.svg.png",
+    time: "18:00",
+  },
+];
 
 export default function Home() {
   const [isEnabledLive, setIsEnabledLive] = useState(false);
   const toggleSwitch = () =>
     setIsEnabledLive((previousState) => !previousState);
+
+  const handleMatchCardPress = () => {
+    router.push("/matchDetails" as any);
+  };
   return (
     <SafeAreaView>
       <View className="m-5">
@@ -23,34 +51,31 @@ export default function Home() {
             />
           </View>
         </View>
-        <View className="flex flex-col mt-5">
-          <Text className="text-lg font-bold">Today</Text>
-          <View className="mt-3">
-            <MatchCard
-              teamOneName="Barcelona"
-              teamOneLogo="https://upload.wikimedia.org/wikipedia/en/thumb/4/47/FC_Barcelona_%28crest%29.svg/1200px-FC_Barcelona_%28crest%29.svg.png"
-              teamTwoName="Manchester United"
-              teamTwoLogo="https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/800px-Manchester_United_FC_crest.svg.png"
-              time="18:00"
-            />
-            <View className="h-[.5px] bg-gray-500 my-2" />
-            <MatchCard
-              teamOneName="Barcelona"
-              teamOneLogo="https://upload.wikimedia.org/wikipedia/en/thumb/4/47/FC_Barcelona_%28crest%29.svg/1200px-FC_Barcelona_%28crest%29.svg.png"
-              teamTwoName="Manchester United"
-              teamTwoLogo="https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/800px-Manchester_United_FC_crest.svg.png"
-              time="18:00"
-            />
-            <View className="h-[.5px] bg-gray-500 my-2" />
-            <MatchCard
-              teamOneName="Barcelona"
-              teamOneLogo="https://upload.wikimedia.org/wikipedia/en/thumb/4/47/FC_Barcelona_%28crest%29.svg/1200px-FC_Barcelona_%28crest%29.svg.png"
-              teamTwoName="Manchester United"
-              teamTwoLogo="https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/800px-Manchester_United_FC_crest.svg.png"
-              time="18:00"
-            />
+        <ScrollView
+          style={{
+            marginBottom: 60,
+          }}
+        >
+          <View className="flex flex-col mt-5">
+            <Text className="text-lg font-bold">Today</Text>
+            <View className="mt-3">
+              {matches.map((match, index) => (
+                <TouchableOpacity key={match.id} onPress={handleMatchCardPress}>
+                  <MatchCard
+                    teamOneName={match.teamOneName}
+                    teamOneLogo={match.teamOneLogo}
+                    teamTwoName={match.teamTwoName}
+                    teamTwoLogo={match.teamTwoLogo}
+                    time={match.time}
+                  />
+                  {index !== matches.length - 1 && (
+                    <View className="h-[.5px] bg-gray-500 my-2" />
+                  )}
+                </TouchableOpacity>
+              ))}
+            </View>
           </View>
-        </View>
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
---
+++ app/matchDetails/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function MatchDetails() {
+  return (
+    <SafeAreaView>
+      <View className="m-5">
+        <Text className="text-3xl font-bold">Match Details</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
